add85d6dfa81d6a54a630541fabb0dce105e262e
Mo Amini <mo.amini@nav.no>
2019-10-17 15:06:25 +0200

    fjerner listener i Q1

--- Jenkinsfile
+++ Jenkinsfile
@@ -66,37 +66,37 @@ node {
 //            ])
 //        }
 //
-        stage("deploy Q1") {
-                    def version = sh(script: 'git describe --abbrev=0', returnStdout: true).trim()
-                    build([
-                            job       : 'nais-deploy-pipeline',
-                            wait      : true,
-                            parameters: [
-                                    string(name: 'APP', value: "eessi-pensjon-begrens-innsyn"),
-                                    string(name: 'REPO', value: "navikt/eessi-pensjon-begrens-innsyn"),
-                                    string(name: 'VERSION', value: version),
-                                    string(name: 'DEPLOY_REF', value: version),
-                                    string(name: 'NAMESPACE', value: 'q1'),
-                                    string(name: 'DEPLOY_ENV', value: 'q1')
-                            ]
-                    ])
-                }
+//        stage("deploy Q1") {
+//                    def version = sh(script: 'git describe --abbrev=0', returnStdout: true).trim()
+//                    build([
+//                            job       : 'nais-deploy-pipeline',
+//                            wait      : true,
+//                            parameters: [
+//                                    string(name: 'APP', value: "eessi-pensjon-begrens-innsyn"),
+//                                    string(name: 'REPO', value: "navikt/eessi-pensjon-begrens-innsyn"),
+//                                    string(name: 'VERSION', value: version),
+//                                    string(name: 'DEPLOY_REF', value: version),
+//                                    string(name: 'NAMESPACE', value: 'q1'),
+//                                    string(name: 'DEPLOY_ENV', value: 'q1')
+//                            ]
+//                    ])
+//                }
 
-//        stage("deploy Q2") {
-//            def version = sh(script: 'git describe --abbrev=0', returnStdout: true).trim()
-//            build([
-//                    job       : 'nais-deploy-pipeline',
-//                    wait      : true,
-//                    parameters: [
-//                            string(name: 'APP', value: "eessi-pensjon-begrens-innsyn"),
-//                            string(name: 'REPO', value: "navikt/eessi-pensjon-begrens-innsyn"),
-//                            string(name: 'VERSION', value: version),
-//                            string(name: 'DEPLOY_REF', value: version),
-//                            string(name: 'NAMESPACE', value: 'q2'),
-//                            string(name: 'DEPLOY_ENV', value: 'q2')
-//                    ]
-//            ])
-//        }
+        stage("deploy Q2") {
+            def version = sh(script: 'git describe --abbrev=0', returnStdout: true).trim()
+            build([
+                    job       : 'nais-deploy-pipeline',
+                    wait      : true,
+                    parameters: [
+                            string(name: 'APP', value: "eessi-pensjon-begrens-innsyn"),
+                            string(name: 'REPO', value: "navikt/eessi-pensjon-begrens-innsyn"),
+                            string(name: 'VERSION', value: version),
+                            string(name: 'DEPLOY_REF', value: version),
+                            string(name: 'NAMESPACE', value: 'q2'),
+                            string(name: 'DEPLOY_ENV', value: 'q2')
+                    ]
+            ])
+        }
 
         github.commitStatus("success", "navikt/eessi-pensjon-begrens-innsyn", appToken, commitHash)
     } catch (err) {
--- src/test/kotlin/no/nav/eessi/pensjon/listeners/SedListenerTest.kt
+++ src/test/kotlin/no/nav/eessi/pensjon/listeners/SedListenerTest.kt
@@ -1,70 +1,70 @@
-//package no.nav.eessi.pensjon.listeners
-//
-//import com.nhaarman.mockitokotlin2.times
-//import com.nhaarman.mockitokotlin2.verify
-//import no.nav.eessi.pensjon.begrens.innsyn.BegrensInnsynService
-//import org.apache.kafka.clients.consumer.ConsumerRecord
-//import org.junit.jupiter.api.BeforeEach
-//import org.junit.jupiter.api.Disabled
-//import org.junit.jupiter.api.Test
-//import org.junit.jupiter.api.assertThrows
-//import org.junit.jupiter.api.extension.ExtendWith
-//import org.mockito.Mock
-//import org.mockito.junit.jupiter.MockitoExtension
-//import org.springframework.kafka.support.Acknowledgment
-//import java.nio.file.Files
-//import java.nio.file.Paths
-//
-//@ExtendWith(MockitoExtension::class)
-//class SedListenerTest {
-//
-//    @Mock
-//    lateinit var acknowledgment: Acknowledgment
-//
-//    @Mock
-//    lateinit var cr: ConsumerRecord<String, String>
-//
-//    @Mock
-//    lateinit var begrensInnsynService: BegrensInnsynService
-//
-//    lateinit var sedListener: SedListener
-//
-//    @BeforeEach
-//    fun setup() {
-//        sedListener = SedListener(begrensInnsynService)
-//    }
-//
-//    @Test
-//    fun `gitt en gyldig sedHendelse når sedSendt hendelse konsumeres så ack melding`() {
-//        sedListener.consumeSedSendt(String(Files.readAllBytes(Paths.get("src/test/resources/sed/P_BUC_01.json"))),cr,  acknowledgment)
-//        verify(acknowledgment).acknowledge()
-//    }
-//
-//    @Test
-//    fun `gitt en gyldig sedHendelse når sedMottatt hendelse konsumeres så så ack melding`() {
-//        sedListener.consumeSedMottatt(String(Files.readAllBytes(Paths.get("src/test/resources/sed/P_BUC_01.json"))),cr, acknowledgment)
-//        verify(acknowledgment).acknowledge()
-//    }
-//
-//    @Disabled //TODO
-//    @Test
-//    fun `gitt en exception ved sedSendt så kastes RunTimeException og meldig blir IKKE ack'et`() {
-//       //doThrow(MockitoException("Boom!")).`when`(jouralforingService).journalfor(eq("Explode!"), any())
-//
-//        assertThrows<RuntimeException> {
-//            sedListener.consumeSedSendt("Explode!",cr, acknowledgment)
-//        }
-//        verify(acknowledgment, times(0)).acknowledge()
-//    }
-//
-//    @Disabled //TODO
-//    @Test
-//    fun `gitt en exception ved sedMottatt så kastes RunTimeException og meldig blir IKKE ack'et`() {
-//        //doThrow(MockitoException("Boom!")).`when`(jouralforingService).journalfor(eq("Explode!"), any())
-//
-//        assertThrows<RuntimeException> {
-//            sedListener.consumeSedMottatt("Explode!",cr, acknowledgment)
-//        }
-//        verify(acknowledgment, times(0)).acknowledge()
-//    }
-//}
+package no.nav.eessi.pensjon.listeners
+
+import com.nhaarman.mockitokotlin2.times
+import com.nhaarman.mockitokotlin2.verify
+import no.nav.eessi.pensjon.begrens.innsyn.BegrensInnsynService
+import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.Mock
+import org.mockito.junit.jupiter.MockitoExtension
+import org.springframework.kafka.support.Acknowledgment
+import java.nio.file.Files
+import java.nio.file.Paths
+
+@ExtendWith(MockitoExtension::class)
+class SedListenerTest {
+
+    @Mock
+    lateinit var acknowledgment: Acknowledgment
+
+    @Mock
+    lateinit var cr: ConsumerRecord<String, String>
+
+    @Mock
+    lateinit var begrensInnsynService: BegrensInnsynService
+
+    lateinit var sedListener: SedListener
+
+    @BeforeEach
+    fun setup() {
+        sedListener = SedListener(begrensInnsynService)
+    }
+
+    @Test
+    fun `gitt en gyldig sedHendelse når sedSendt hendelse konsumeres så ack melding`() {
+        sedListener.consumeSedSendt(String(Files.readAllBytes(Paths.get("src/test/resources/sed/P_BUC_01.json"))),cr,  acknowledgment)
+        verify(acknowledgment).acknowledge()
+    }
+
+    @Test
+    fun `gitt en gyldig sedHendelse når sedMottatt hendelse konsumeres så så ack melding`() {
+        sedListener.consumeSedMottatt(String(Files.readAllBytes(Paths.get("src/test/resources/sed/P_BUC_01.json"))),cr, acknowledgment)
+        verify(acknowledgment).acknowledge()
+    }
+
+    @Disabled //TODO
+    @Test
+    fun `gitt en exception ved sedSendt så kastes RunTimeException og meldig blir IKKE ack'et`() {
+       //doThrow(MockitoException("Boom!")).`when`(jouralforingService).journalfor(eq("Explode!"), any())
+
+        assertThrows<RuntimeException> {
+            sedListener.consumeSedSendt("Explode!",cr, acknowledgment)
+        }
+        verify(acknowledgment, times(0)).acknowledge()
+    }
+
+    @Disabled //TODO
+    @Test
+    fun `gitt en exception ved sedMottatt så kastes RunTimeException og meldig blir IKKE ack'et`() {
+        //doThrow(MockitoException("Boom!")).`when`(jouralforingService).journalfor(eq("Explode!"), any())
+
+        assertThrows<RuntimeException> {
+            sedListener.consumeSedMottatt("Explode!",cr, acknowledgment)
+        }
+        verify(acknowledgment, times(0)).acknowledge()
+    }
+}
