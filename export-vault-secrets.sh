136f186b7c1a5cfe15e408178a2bd785456565bf
moamini <mo.amini@nav.no>
2020-01-20 10:22:22 +0100

    repo.adeo gone

--- export-vault-secrets.sh
+++ export-vault-secrets.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
+echo "Sjekker eessi-pensjon-begrens-innsyn srvPassord"
 if test -f /var/run/secrets/nais.io/srveessi-pensjon-j/password;
 then
-    echo "Setting SYSTEMBRUKER_PASSWORD"
+  echo "Setter eessi-pensjon-begrens-innsyn srvPassord"
     export  SYSTEMBRUKER_PASSWORD=$(cat /var/run/secrets/nais.io/srveessi-pensjon-j/password)
 fi
 
+echo "Sjekker eessi-pensjon-begrens-innsyn srvUsername"
 if test -f /var/run/secrets/nais.io/srveessi-pensjon-j/username;
 then
-    echo "Setting SYSTEMBRUKER_USERNAME"
+    echo "Setter eessi-pensjon-begrens-innsyn srvUsername"
     export  SYSTEMBRUKER_USERNAME=$(cat /var/run/secrets/nais.io/srveessi-pensjon-j/username)
 fi
