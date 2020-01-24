ed10295992f4f8bc466b5e527ad9c1ecf78ceb34
moamini <mo.amini@nav.no>
2020-01-24 12:37:37 +0100

    byttet fra username til srvusernme

--- export-vault-secrets.sh
+++ export-vault-secrets.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
 echo "Sjekker eessi-pensjon-begrens-innsyn srvPassord"
-if test -f /var/run/secrets/nais.io/srveessi-pensjon-journalforing/srvpassword;
+if test -f /var/run/secrets/nais.io/srveessi-pensjon-journalforing/password;
 then
   echo "Setter eessi-pensjon-begrens-innsyn srvPassord"
-    export password=$(cat /var/run/secrets/nais.io/srveessi-pensjon-journalforing/srvpassword)
+    export srvpassword=$(cat /var/run/secrets/nais.io/srveessi-pensjon-journalforing/password)
 fi
 
 echo "Sjekker eessi-pensjon-begrens-innsyn srvUsername"
-if test -f /var/run/secrets/nais.io/srveessi-pensjon-journalforing/srvusername;
+if test -f /var/run/secrets/nais.io/srveessi-pensjon-journalforing/username;
 then
     echo "Setter eessi-pensjon-begrens-innsyn srvUsername"
-    export username=$(cat /var/run/secrets/nais.io/srveessi-pensjon-journalforing/srvusername)
+    export srvusername=$(cat /var/run/secrets/nais.io/srveessi-pensjon-journalforing/username)
 fi
