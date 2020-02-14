a486029c878c2ce2c2c68d8bdb9b7ff3c688e141
Oyvind Buer <oyvindbuer@gmail.com>
2020-02-14 12:07:52 +0100

    Using faultString to handle F002001F

--- src/main/kotlin/no/nav/eessi/pensjon/services/personv3/PersonV3Service.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/services/personv3/PersonV3Service.kt
@@ -40,10 +40,7 @@ class PersonV3Service(
                 logger.warn("PersonV3: Kunne ikke hente person, ikke funnet", pif)
                 null
             } catch (sfe: SOAPFaultException) {
-                logger.warn("PersonV3: faultCode: ${sfe.fault.faultCode}")
-                logger.warn("PersonV3: faultString: ${sfe.fault.faultString}")
-                logger.warn("PersonV3: faultCodeAsName: ${sfe.fault.faultCodeAsName}")
-                if (sfe.fault.faultCode == "F002001F") {
+                if (sfe.fault.faultString.contains("F002001F")) {
                     logger.warn("PersonV3: Kunne ikke hente person, ugyldig input", sfe)
                     null
                 } else {
@@ -77,9 +74,6 @@ class PersonV3Service(
         configureRequestSamlToken(service)
     }
 }
-
-@ResponseStatus(value = HttpStatus.NOT_FOUND)
-class PersonV3IkkeFunnetException(message: String?): Exception(message)
 
 @ResponseStatus(value = HttpStatus.FORBIDDEN)
 class PersonV3SikkerhetsbegrensningException(message: String?): Exception(message)
