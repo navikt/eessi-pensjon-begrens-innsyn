dec3f88f5732f1266c0a088e90291dfd6472f55b
mariampervez <mariam.pervez@nav.no>
2022-01-25 16:02:46 +0100

    legger til @Unprotected på DiagnosticsController
    Co-authored-by: Daniel Skarpås <dskarpas@gmail.com>
    Co-authored-by: Jens Chr. Madsen <jens.christian.madsen@nav.no>
    Co-authored-by: Mo Amini <mo.amini@nav.no>

--- src/main/kotlin/no/nav/eessi/pensjon/health/DiagnosticsController.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/health/DiagnosticsController.kt
@@ -1,5 +1,6 @@
 package no.nav.eessi.pensjon.health
 
+import no.nav.security.token.support.core.api.Unprotected
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.http.ResponseEntity
@@ -7,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import java.time.Instant
 
-
+@Unprotected
 @RestController
 class DiagnosticsController {
 
