0ee0c9b726aa569ece96c02d8a81a56fdef83c9f
Mo Amini <mo.amini@nav.no>
2022-01-28 12:01:28 +0100

    logger subject

--- src/main/kotlin/no/nav/eessi/pensjon/config/Oauth2Config.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/config/Oauth2Config.kt
@@ -50,7 +50,7 @@ class OAuth2Configuration {
             val response = oAuth2AccessTokenService.getAccessToken(clientProperties)
             println("token: ${response.accessToken}")
             request.headers.setBearerAuth(response.accessToken)
-            println("sub: ${JWTClaimsSet.parse(Base64.getDecoder().decode(response.accessToken).toString()).subject}")
+            println("sub: ${JWTClaimsSet.parse(Base64.getDecoder().decode(response.accessToken).toString().split(".").first()).subject}")
             execution.execute(request, body!!)
         }
     }
