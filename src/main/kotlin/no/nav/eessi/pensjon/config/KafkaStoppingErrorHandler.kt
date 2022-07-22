10156b0a5043ac9f975c9ea3e337bc7c816039ac
Daniel Skarpås <dskarpas@gmail.com>
2022-07-22 15:45:40 +0200

    f endrer depricated ContainerAwareErrorHandler ->  CommonErrorHandler i kafkakonfig for alle prosjekter

--- src/main/kotlin/no/nav/eessi/pensjon/config/KafkaStoppingErrorHandler.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/config/KafkaStoppingErrorHandler.kt
@@ -5,38 +5,32 @@ import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Profile
 import org.springframework.kafka.listener.CommonContainerStoppingErrorHandler
-import org.springframework.kafka.listener.ContainerAwareErrorHandler
+import org.springframework.kafka.listener.CommonErrorHandler
 import org.springframework.kafka.listener.MessageListenerContainer
 import org.springframework.stereotype.Component
-import java.io.PrintWriter
 import java.io.StringWriter
-
 
 @Profile("prod")
 @Component
-class KafkaStoppingErrorHandler : ContainerAwareErrorHandler {
+class KafkaStoppingErrorHandler : CommonErrorHandler {
     private val logger = LoggerFactory.getLogger(KafkaStoppingErrorHandler::class.java)
     private val stopper = CommonContainerStoppingErrorHandler()
 
-    override fun handle(
-        thrownException: java.lang.Exception,
-        records: MutableList<ConsumerRecord<*, *>>?,
+    override fun handleRecord(
+        thrownException: Exception,
+        record: ConsumerRecord<*, *>,
         consumer: Consumer<*, *>,
-        container: MessageListenerContainer
-    ) {
-        val stacktrace = StringWriter()
-        thrownException.printStackTrace(PrintWriter(stacktrace))
+        container: MessageListenerContainer) {
 
-        logger.error("En feil oppstod under kafka konsumering av meldinger: \n" +
-                textListingOf(records ?: emptyList()) +
-                "\nStopper containeren ! Restart er nødvendig for å fortsette konsumering, $stacktrace")
-        stopper.handleRemaining(thrownException, records?: emptyList(), consumer, container)
+        val stacktrace = StringWriter()
+
+        logger.error("En feil oppstod under kafka konsumering av meldinger: \n ${hentMeldinger(record)} \n" +
+                "Stopper containeren ! Restart er nødvendig for å fortsette konsumering, $stacktrace")
+        stopper.handleRecord(thrownException, record, consumer, container)
+
     }
 
-    fun textListingOf(records: List<ConsumerRecord<*, *>>) =
-        records.map { vask11sifre(it.toString()) }.joinToString(separator = "\n") {
-            "--------------------------------------------------------------------------------\n$it"
-        }
+    fun hentMeldinger(records: ConsumerRecord<*, *>) = "-" .repeat(20) +  vask11sifre(records.toString())
 
     // TODO Finn gjerne en bedre måte
     private fun vask11sifre(tekst: String) = tekst.replace(Regex("""\d{11}"""), "***")
